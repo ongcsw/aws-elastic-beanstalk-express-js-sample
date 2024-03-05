9a02036cc979a928c8519bda4407a7226479ff77
ongcsw <162139527+ongcsw@users.noreply.github.com>
2024-03-05 10:39:12 +0800

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Seve the World!'));
+app.get('/', (req, res) => res.send('New Message here! hello everyone!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
